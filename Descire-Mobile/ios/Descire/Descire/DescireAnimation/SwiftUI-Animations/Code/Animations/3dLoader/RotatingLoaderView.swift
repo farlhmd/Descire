6364cfea85b274e0740ed870bd02238349de206f
farlhmd <m.farhanilhamdi@icloud.com>
2020-11-30 02:02:25 +0700

    updated ios app

---
+++ Descire-Mobile/ios/Descire/Descire/DescireAnimation/SwiftUI-Animations/Code/Animations/3dLoader/RotatingLoaderView.swift
@@ -0,0 +1,155 @@
+//
+//  3dLoader.swift
+//  SwiftUI-Animations
+//
+//  Created by Shubham Singh on 17/10/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import SwiftUI
+
+enum RotationState: CaseIterable {
+    case initialLeading
+    case finalLeading
+    case initialTrailing
+    case finalTrailing
+    
+    // Degree, Offset, Anchor, Axis
+    var rotationValues: (Double, CGFloat, UnitPoint, CGFloat) {
+        switch self {
+        case .initialLeading:
+            return (90, 260, UnitPoint.leading, 1)
+        case .finalLeading:
+            return (0, 0, UnitPoint.leading, 1)
+        case .initialTrailing:
+            return (0, 0,UnitPoint.trailing, -1)
+        case .finalTrailing:
+            return (90, -260 ,UnitPoint.trailing, -1)
+        }
+    }
+}
+
+struct RotatingLoaderView: View {
+    
+    // MARK:- variables
+    @State var firstViewDegree: Double = RotationState.initialTrailing.rotationValues.0
+    @State var firstViewOffset: CGFloat = RotationState.initialTrailing.rotationValues.1
+    @State var firstViewAnchor: UnitPoint = RotationState.initialTrailing.rotationValues.2
+    @State var firstViewYAxis: CGFloat = RotationState.initialTrailing.rotationValues.3
+    
+    @State var secondViewDegree:Double = RotationState.initialLeading.rotationValues.0
+    @State var secondViewOffset:CGFloat = RotationState.initialLeading.rotationValues.1
+    @State var secondViewAnchor: UnitPoint = RotationState.initialLeading.rotationValues.2
+    @State var secondViewYAxis: CGFloat = RotationState.initialLeading.rotationValues.3
+    
+    
+    @State var timerDuration: TimeInterval = 3.5
+    @State var animationDuration: TimeInterval = 1.5
+    @State var animateTrail: Bool = false
+    @State var showFlickeringViews: Bool = false
+    
+    @State var counter = 0
+    
+    // MARK:- views
+    var body: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            ZStack {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color.white)
+                        .frame(width: 260, height: 260)
+                    DashedLoaderView()
+                        .frame(width: 140, height: 140)
+                }.rotation3DEffect(.degrees(secondViewDegree), axis: (x: 0, y: secondViewYAxis, z: 0), anchor: secondViewAnchor, anchorZ: 0, perspective: 0.1)
+                .offset(CGSize(width: secondViewOffset, height: 0))
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color.materialBlack)
+                        .frame(width: 260, height: 260)
+                    if (counter == 0 || counter == 1) {
+                        RectangleLoaderView()
+                            .frame(width: 140, height: 140)
+                    } else  {
+                        DotsLoaderView()
+                            .frame(width: 140, height: 140)
+                    }
+                }.rotation3DEffect(.degrees(firstViewDegree), axis: (x: 0, y: firstViewYAxis, z: 0), anchor: firstViewAnchor, anchorZ: 0, perspective: 0.1)
+                .offset(x: firstViewOffset, y: 0)
+                if (showFlickeringViews) {
+                    FlickeringView(backgroundColor: Color.white, initialOffset: CGSize(width: 200, height: -300), initialSize: CGSize(width: 200, height: 44), finalSize: CGSize(width: 100, height: 44), finalOffset: CGSize(width: -120, height: -300), fadeDuration: 0.6)
+                    
+                    FlickeringView(backgroundColor: Color.black, initialOffset: CGSize(width: 200, height: -100), initialSize: CGSize(width: 300, height: 32), finalSize: CGSize(width: 44, height: 32), finalOffset: CGSize(width: -80, height: -100), fadeDuration: 0.5)
+                    
+                    FlickeringView(backgroundColor: Color.white, initialOffset: CGSize(width: 200, height: 250), initialSize: CGSize(width: 40, height: 40), finalSize: CGSize(width: 40, height: 40), finalOffset: CGSize(width: -80, height: 250), fadeDuration: 0.75)
+                }
+            }
+            VStack{
+                Spacer()
+                HStack {
+                    Spacer()
+                    Text("@Shubham_iosdev")
+                        .foregroundColor(.white)
+                        .font(.system(size: 28, weight: .medium, design: .monospaced))
+                        .opacity(0.3)
+                }.padding(.trailing, 16)
+            }
+        }.onAppear() {
+            Timer.scheduledTimer(withTimeInterval: timerDuration, repeats: false) { _ in
+                withAnimation(Animation.easeOut(duration: animationDuration)) {
+                    self.setValuesOnState(rotation1: .finalTrailing, rotation2: .finalLeading)
+                    counter += 1
+                    rotateCube()
+                    showFlickeringViews.toggle()
+                }
+            }
+        }
+    }
+    
+    // MARK:- functions
+    func setValuesOnState(rotation1: RotationState, rotation2: RotationState) {
+        self.firstViewDegree = rotation1.rotationValues.0
+        self.firstViewOffset = rotation1.rotationValues.1
+        self.firstViewAnchor = rotation1.rotationValues.2
+        self.firstViewYAxis = rotation1.rotationValues.3
+        
+        self.secondViewDegree = rotation2.rotationValues.0
+        self.secondViewOffset = rotation2.rotationValues.1
+        self.secondViewAnchor = rotation2.rotationValues.2
+        self.secondViewYAxis = rotation2.rotationValues.3
+    }
+    
+    func stepCounter() {
+        if (counter == 3) {
+            counter = -1
+        }
+        counter += 1
+    }
+    
+    func rotateCube() {
+        Timer.scheduledTimer(withTimeInterval: timerDuration, repeats: true) { _ in
+            stepCounter()
+            showFlickeringViews.toggle()
+            if (animateTrail) {
+                self.setValuesOnState(rotation1: .initialTrailing, rotation2: .initialLeading)
+            } else {
+                self.setValuesOnState(rotation1: .initialLeading, rotation2: .initialTrailing)
+            }
+            withAnimation(Animation.easeOut(duration: animationDuration)) {
+                if (animateTrail) {
+                    self.setValuesOnState(rotation1: .finalTrailing, rotation2: .finalLeading)
+                } else {
+                    self.setValuesOnState(rotation1: .finalLeading, rotation2: .finalTrailing)
+                }
+            }
+            self.animateTrail.toggle()
+        }
+    }
+}
+
+struct _dLoader_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatingLoaderView()
+    }
+}
---
+++ Descire-Mobile/ios/Descire/Descire/DescireAnimation/SwiftUI-Animations/Code/Utils/Colors.swift
@@ -0,0 +1,45 @@
+//
+//  Colors.swift
+//  SwiftUI-Animations
+//
+//  Created by Shubham Singh on 08/08/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let chatBackground: Color = Color(r: 41, g: 121, b: 255.0)
+    static let buttonBackground: Color = Color(r: 144.0, g: 202.0, b: 249.0)
+    
+    static let wifiBackground: Color = Color(r: 5, g: 23, b: 46)
+    static let wifiShadow: Color = Color(r: 13, g: 50, b: 125)
+    
+    static let wifiConnected: Color = Color(r: 170, g: 255, b: 197)
+    
+    static let expandingBackground: Color = Color(r: 3, g: 247, b: 235)
+    static let expandingAccent: Color = Color(r: 186, g: 38, b: 75)
+    
+    static let circleTrackStart: Color = Color(r: 237, g: 242, b: 255)
+    static let circleTrackEnd: Color = Color(r: 235, g: 248, b: 255)
+    
+    static let circleRoundStart: Color = Color(r: 71, g: 198, b: 255)
+    static let circleRoundEnd: Color = Color(r: 90, g: 131, b: 255)
+    
+    static let pillColor: Color = Color(r: 242, g: 53, b: 174)
+    
+    static let likeBackground: Color = Color(r: 49, g: 28, b: 78)
+    static let likeOverlay: Color = Color(r: 64, g: 49, b: 82)
+    
+    static let likeColor: Color = Color(r: 254, g: 140, b: 100)
+    
+    static let submitColor: Color = Color(r: 110, g: 80, b: 249)
+    
+    static let materialBlack: Color = Color(r: 18, g: 18, b: 18)
+    static let offWhite: Color = Color(r: 225, g: 225, b: 235)
+    
+    init(r: Double, g: Double, b: Double) {
+        self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0)
+    }
+}
